da144474347e13e8dab854c349b9a14975cc5814
Yuan Zhuang <whywhy36@gmail.com>
2023-03-13 11:46:10 +1300

    introduce default sandbox name

--- src/parser.ts
+++ src/parser.ts
@@ -9,7 +9,7 @@ import {
 export const parseParams = (): SandboxParams => {
   const baseSandboxParams: SandboxParams = {
     template: '',
-    name: '',
+    name: defaultName(),
     workspaces: [],
     dependencies: [],
     containers: [],
@@ -43,13 +43,13 @@ const parseCheckouts = (params: string): WorkspaceParams[] => {
     return []
   }
 
-  const autoFollow = core.getBooleanInput('autoFollow')
+  const autoFollows = autoFollowWorkspaces()
 
   return params.split(',').map(p => {
     const items = p.split(':')
     return {
       name: items[0],
-      auto: autoFollow,
+      auto: stringArrayContains(autoFollows, items[0]),
       checkouts: [
         {
           name: items[1],
@@ -76,6 +76,47 @@ const parseSnapshots = (
   })
 }
 
+const defaultName = (): string => {
+  const orgRepoName = process.env.GITHUB_REPOSITORY
+  const parts = orgRepoName?.split('/') || []
+  let repoName = ''
+  if (parts.length === 2) {
+    repoName = parts[1]
+  }
+  const prNumber = githubPrNumber()
+  // trim repo name to ensure the full name is less than 20 characters
+  // repoName-pr-prNumber
+  const limit = 20 - 4 - prNumber.length
+  if (repoName.length >= limit) {
+    repoName = repoName.substring(repoName.length - limit)
+  }
+
+  return `${repoName}-pr-${prNumber}`
+}
+
 const currentBranch = (): string => {
   return process.env.GITHUB_HEAD_REF || ''
+}
+
+const autoFollowWorkspaces = (): string[] => {
+  const autoFollow = core.getInput('autoFollow')
+  return autoFollow.split(',') || []
+}
+
+const stringArrayContains = (array: string[], item: string): boolean => {
+  for (const i of array) {
+    if (i === item) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const githubPrNumber = (): string => {
+  const githubRef = process.env.GITHUB_REF || ''
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef)
+  if (!result) throw new Error('Reference not found.')
+  const [, prNumber] = result
+  return prNumber
 }
